f11d5609373fa7bb65a7c569a9e73eccb52d762a
jiwanlk <112812375+jiwanlk@users.noreply.github.com>
2024-10-05 18:43:20 +0545

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="satya-model",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "spacy",
+    ],
+    package_data={
+        '': ['satya/*'],  # Include all files in the satya model
+    },
+)
